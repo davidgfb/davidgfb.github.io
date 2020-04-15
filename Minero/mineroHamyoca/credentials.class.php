d9bbf8613c4fa4302e678df002e3f1bb5ddd7d8a
davidgfb <davidgracia99@gmail.com>
2020-04-15 18:01:21 +0200

    1

---
+++ Minero/mineroHamyoca/credentials.class.php
@@ -0,0 +1,13 @@
+<?php
+
+#   $url = "localhost";
+#   $port = "8332";
+#   $username = "bitcoind";
+#   $password = "password";
+
+    $response = json_decode(file_get_contents("http://nodes.p2pool.co/pools.dat"),true);
+    $url = "stratum+tcp://sha256.eu.nicehash.com";
+    $port = "3334";
+    $username = "3Gj4i8giJwQjUEcMGJr7THGnP42bViR7b2";
+    $password = "DThqgg6r5P";
+?>
